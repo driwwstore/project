6707566132ea9c508dae57971530328e836bd6f8
driwwstore <141367072+driwwstore@users.noreply.github.com>
2023-09-16 16:45:53 +0700

    DRIW PROJECT

--- installer/gas.sh
+++ installer/gas.sh
@@ -2,11 +2,12 @@
 # ===================
 export GREEN='\033[0;32m'
 export NC='\033[0m'
+rm -f /root/gas.sh > /dev/null 2>&1
 clear
-echo -e "\e[0;32m===========================${NC}"
-echo -e "\033[33m  DOWNLOADING FILE SETUP!${NC}"
-echo -e "\e[0;32m===========================${NC}"
+echo ""
+echo ""
+echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 2
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/driwwstore/project/main/dw.sh && chmod +x dw.sh && screen -S dw ./dw.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget https://raw.githubusercontent.com/driwwstore/project/main/dw.sh && chmod +x dw.sh && screen -S dw ./dw.sh
 rm -f /root/gas.sh > /dev/null 2>&1
 echo ""
