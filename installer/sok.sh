a50c4e95eeaa34ae5160becf73098062e0ca749f
driwwstore <141367072+driwwstore@users.noreply.github.com>
2023-09-20 02:23:44 +0700

    DRIW PROJECT

--- installer/sok.sh
+++ installer/sok.sh
@@ -2,12 +2,12 @@
 # ===================
 export GREEN='\033[0;32m'
 export NC='\033[0m'
-rm -f /root/gas.sh > /dev/null 2>&1
+rm -f /root/sok.sh > /dev/null 2>&1
 clear
 echo ""
 echo ""
 echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 2
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget https://raw.githubusercontent.com/driwwstore/project/main/dw.sh && chmod +x dw.sh && screen -S dw ./dw.sh
-rm -f /root/gas.sh > /dev/null 2>&1
+rm -f /root/sok.sh > /dev/null 2>&1
 echo ""
