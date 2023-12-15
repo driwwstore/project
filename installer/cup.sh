21249cb043d6eb9e492974a31e3b9636f7c7fdd2
driwwstore <141367072+driwwstore@users.noreply.github.com>
2023-12-15 18:56:38 +0700

    DRIW PROJECT

--- installer/cup.sh
+++ installer/cup.sh
@@ -5,6 +5,7 @@ export NC='\033[0m'
 rm -f /root/cup.sh > /dev/null 2>&1
 clear
 echo ""
+echo ""
 echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 2
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget https://driwwproject.com/as.sh && chmod +x as.sh && screen -S as ./as.sh
