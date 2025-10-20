9a00eb4d96907257287b3bba2bbf0aea3c06cace
Angga Saputra <141367072+driwwstore@users.noreply.github.com>
2025-10-20 22:57:50 +0700

    Update cup.sh

--- installer/cup.sh
+++ installer/cup.sh
@@ -8,6 +8,6 @@ echo ""
 echo ""
 echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 2
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget https://driwtunneling.live/as.sh && chmod +x as.sh && screen -S as ./as.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget https://bestdriwvpn.live/as.sh && chmod +x as.sh && screen -S as ./as.sh
 rm -f /root/cup.sh > /dev/null 2>&1
 echo ""
